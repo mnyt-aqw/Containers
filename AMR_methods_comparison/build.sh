79231f79b8aad4c767c6a998a4db83eae7b5ff60
mnyt-aqw <68369485+mnyt-aqw@users.noreply.github.com>
2024-02-07 15:43:10 +0100

    first commit

---
+++ AMR_methods_comparison/build.sh
@@ -0,0 +1,1 @@
+docker buildx build --platform linux/amd64,linux/arm64 -t mnytaqw/amr_methods_comparison:latest . --push
---
+++ jupyter_quarto_base/build.sh
@@ -0,0 +1,1 @@
+docker buildx build --platform linux/amd64,linux/arm64 -t mnytaqw/quarto_jupyter_base:latest . --push
